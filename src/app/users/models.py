21e109ecd10dd56e73d6cce31fdcc3429b6f051a
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-10 15:19:58 +0300

    add BaseUser model

--- src/app/users/models.py
+++ src/app/users/models.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from sqlalchemy import Column, String, Boolean, DATETIME
 
+from src.app.core.models import PKUUIDMixin, Base
+
 
 class BaseUser:
     username = Column(String(length=255), unique=True)
@@ -10,3 +12,9 @@ class BaseUser:
     is_active = Column(Boolean, default=False)
     is_superuser = Column(Boolean, default=False)
     join_date_time = Column(DATETIME, default=datetime.now)
+
+
+class User(PKUUIDMixin, Base):
+    first_name = Column(String(length=255))
+    last_name = Column(String(length=255))
+    birth_date = Column(String, nullable=True)
