fb836b47c8b8b69d65b9748178423a8123327128
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-14 16:54:50 +0300

    add check if postgres up in entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-bash -c "cd app && alembic revision --autogenerate && alembic upgrade head"
-bash -c "cd app && python initial_data.py"
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+
+bash -c "cd app && alembic upgrade head"
 exec "$@"
