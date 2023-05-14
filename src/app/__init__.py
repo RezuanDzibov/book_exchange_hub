81097c3d0c1b687c17b461accbd462c49e14b826
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-14 16:55:35 +0300

    remove models gathering to models module

--- src/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from src.app.core.models import Base
-from src.app.users.models import User
-from src.app.books.models import (
-    Category,
-    Book,
-    UserBook,
-    BookRequest,
-    BookExchange
-)
---
+++ src/app/models.py
@@ -0,0 +1,9 @@
+from core.models import Base
+from users.models import User
+from books.models import (
+    Category,
+    Book,
+    UserBook,
+    BookRequest,
+    BookExchange
+)
